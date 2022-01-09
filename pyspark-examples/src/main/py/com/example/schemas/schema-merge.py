674d654590e4e2d47d6ed82909cbd3fbb105b207
Brijesh K Dhaker <brijeshdhaker@gmail.com>
2022-01-09 21:41:31 +0530

    2022-01-09 update kafka broker for confluent kafka.

--- pyspark-examples/src/main/py/com/example/schemas/schema-merge.py
+++ pyspark-examples/src/main/py/com/example/schemas/schema-merge.py
@@ -9,13 +9,24 @@ spark = SparkSession \
     .appName("pyspark-schema-merge") \
     .getOrCreate()
 
+print("Schema Version - 1 : ")
+df_v1 = spark.read.parquet("file:///home/brijeshdhaker/IdeaProjects/spark-bigdata-examples/data/employee-v1.parquet")
+df_v1.show()
+
+print("Schema Version - 2 : ")
+df_v2 = spark.read.parquet("file:///home/brijeshdhaker/IdeaProjects/spark-bigdata-examples/data/employee-v2.parquet")
+df_v2.show()
+
+print("Schema Version - 3 : ")
+df_v3 = spark.read.parquet("file:///home/brijeshdhaker/IdeaProjects/spark-bigdata-examples/data/employee-v3.parquet")
+df_v3.show()
+
 print("Schema Without Merge : ")
-df_1 = spark.read.parquet("file:///home/brijeshdhaker/IdeaProjects/pyspark-schema-merge/data/employee-*.parquet")
+df_1 = spark.read.parquet("file:///home/brijeshdhaker/IdeaProjects/spark-bigdata-examples/data/employee-*.parquet")
 df_1.show()
 
-
 print("Schema With Merge : ")
-df_2 = spark.read.option("mergeSchema", True).parquet("file:///home/brijeshdhaker/IdeaProjects/pyspark-schema-merge/data/employee-*.parquet")
+df_2 = spark.read.option("mergeSchema", True).parquet("file:///home/brijeshdhaker/IdeaProjects/spark-bigdata-examples/data/employee-*.parquet")
 df_2.show()
 
 spark.stop()
