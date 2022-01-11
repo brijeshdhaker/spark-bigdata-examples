49bad5b33fafcc624da9332c0d8df038be8a3e47
Brijesh K Dhaker <brijeshdhaker@gmail.com>
2022-01-11 11:33:59 +0530

    2022-01-11 Update sandbox-cluster3 configuration

--- spark-examples/src/main/scala/com/spark/salted/SaltedExample.scala
+++ spark-examples/src/main/scala/com/spark/salted/SaltedExample.scala
@@ -6,12 +6,13 @@ import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 
 object SaltedExample {
+
   Logger.getLogger("org").setLevel(Level.OFF)
   Logger.getLogger("akka").setLevel(Level.OFF)
 
-  def main(args:Array[String]): Unit = {
+  def main(args: Array[String]): Unit = {
 
-    val jsonPath = "C:\\Neha\\personal\\Adobe\\Code\\spark_training-master\\data\\datamodeling\\Salted.json"//args(0)
+    val jsonPath = "C:\\Neha\\personal\\Adobe\\Code\\spark_training-master\\data\\datamodeling\\Salted.json" //args(0)
 
     val sparkSession = SparkSession.builder
       .master("local")
@@ -25,7 +26,7 @@ object SaltedExample {
       val group = r.getAs[String]("group")
       val value = r.getAs[Long]("value")
 
-      Seq((group + "_" + 0, value),(group + "_" + 1, value))
+      Seq((group + "_" + 0, value), (group + "_" + 1, value))
     })
 
     val jsonDfRight = sparkSession.read.json(jsonPath)
@@ -45,9 +46,12 @@ object SaltedExample {
     jsonDfLeft.join(jsonDfRight).collect().foreach(r => {
       println("Normal.result:" + r)
     })
+
     println("----")
+
     saltedLeft.join(saltedRight).collect().foreach(r => {
       println("Salted.result:" + r)
     })
+
   }
 }
--- spark-examples/src/main/scala/com/spark/streaming/dstreams/HiveDstreamTransformer.scala
+++ spark-examples/src/main/scala/com/spark/streaming/dstreams/HiveDstreamTransformer.scala
@@ -13,57 +13,90 @@ import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 
 import java.util.UUID
 
-object HiveDstreamTransformer {
+object HiveDstreamTransformer extends App {
 
-  def main(args: Array[String]): Unit = {
-
-    val conf = new SparkConf().setAppName("hive-stream-transformer").setMaster("local[4]")
-    val sc = new SparkContext(conf)
-    sc.setLogLevel("WARN")
-    val ss = SparkSession.builder().getOrCreate()
-
-    val ssc = new StreamingContext(sc, Durations.seconds(10))
-
-    val kafkaParams = Map[String, Object](
-      "bootstrap.servers" -> "kafka-broker:9092",
-      "key.deserializer" -> classOf[StringDeserializer],
-      "value.deserializer" -> classOf[StringDeserializer],
-      "group.id" -> "hive-dstream-cg",
-      "auto.offset.reset" -> "latest",
-      "enable.auto.commit" -> (false: java.lang.Boolean)
-    )
-
-    val topics = Array("tweeter-tweets")
-    val stream = KafkaUtils.createDirectStream[String, String](ssc, PreferConsistent, Subscribe[String, String](topics, kafkaParams))
-
-    val lines = stream.map(record => (record.key, record.value))
-
-    lines.foreachRDD(rdd => {
-
-      // 1 - Create a SchemaRDD object from the rdd and specify the schema
-      val recordsRDD = rdd.map(x => (Row(UUID.randomUUID().toString, x._2, x._2.split(" ").size, x._2.size)))
-      val schema = StructType( Array(
-        StructField("uuid", StringType, true),
-        StructField("text", StringType, true),
-        StructField("words", IntegerType, true),
-        StructField("length", IntegerType, true)
-      ))
-
-      val recordsDF = ss.createDataFrame(recordsRDD, schema)
-      recordsDF.show()
-
-      // 2 - register it as a spark sql table
-      //recordsDF.registerTempTable("sparktable")
-
-      // 3 - qry sparktable to produce another SchemaRDD object of the data needed 'finalParquet'. and persist this as parquet files
-      //val finalParquet = ss.sql("sql")
-      //finalParquet.write.saveAsTable("")
-      recordsDF.write.format("orc").mode(SaveMode.Append).saveAsTable("default.tweeter_tweets")
-
-    })
-
-    ssc.start()
-    ssc.awaitTermination()
+  def processRecord(x:Row): Unit ={
 
   }
+
+  def processRecord(rdd: RDD[(String,String)]): Unit ={
+
+    // 1 - Create a SchemaRDD object from the rdd and specify the schema
+    val recordsRDD = rdd.map(x => (Row(UUID.randomUUID().toString, x._2, x._2.split(" ").size, x._2.size)))
+
+    val schema = StructType( Array(
+      StructField("uuid", StringType, true),
+      StructField("text", StringType, true),
+      StructField("words", IntegerType, true),
+      StructField("length", IntegerType, true)
+    ))
+    val recordsDF = ss.createDataFrame(recordsRDD, schema)
+    recordsDF.show()
+    // 2 - register it as a spark sql table
+    //recordsDF.registerTempTable("sparktable")
+    // 3 - qry sparktable to produce another SchemaRDD object of the data needed 'finalParquet'. and persist this as parquet files
+    //val finalParquet = ss.sql("sql")
+    //finalParquet.write.saveAsTable("")
+    // Turn on flag for Hive Dynamic Partitioning
+    ss.conf.set("hive.exec.dynamic.partition", "true")
+    ss.conf.set("hive.exec.dynamic.partition.mode", "nonstrict")
+    //recordsDF.write.mode("overwrite").saveAsTable("default.tweeter_tweets")
+    recordsDF.write.mode(SaveMode.Append).saveAsTable("tweeter_tweets")
+  }
+
+  val conf = new SparkConf().setAppName("hive-stream-transformer").setMaster("local[4]")
+  val sc = new SparkContext(conf)
+  sc.setLogLevel("WARN")
+  val ss = SparkSession.builder().enableHiveSupport().getOrCreate()
+
+  val ssc = new StreamingContext(sc, Durations.seconds(10))
+
+  val kafkaParams = Map[String, Object](
+    "bootstrap.servers" -> "kafka-broker:9092",
+    "key.deserializer" -> classOf[StringDeserializer],
+    "value.deserializer" -> classOf[StringDeserializer],
+    "group.id" -> "hive-dstream-cg",
+    "auto.offset.reset" -> "latest",
+    "enable.auto.commit" -> (false: java.lang.Boolean)
+  )
+
+  val topics = Array("tweeter-tweets")
+  val stream = KafkaUtils.createDirectStream[String, String](ssc, PreferConsistent, Subscribe[String, String](topics, kafkaParams))
+
+  val lines = stream.map(record => (record.key, record.value))
+
+  lines.foreachRDD(rdd => processRecord(rdd))
+
+  /*
+  lines.foreachRDD(rdd => {
+
+    // 1 - Create a SchemaRDD object from the rdd and specify the schema
+    val recordsRDD = rdd.map(x => (Row(UUID.randomUUID().toString, x._2, x._2.split(" ").size, x._2.size)))
+    recordsRDD.map(processRecord)
+
+    val schema = StructType( Array(
+      StructField("uuid", StringType, true),
+      StructField("text", StringType, true),
+      StructField("words", IntegerType, true),
+      StructField("length", IntegerType, true)
+    ))
+
+    val recordsDF = ss.createDataFrame(recordsRDD, schema)
+    recordsDF.show()
+
+    // 2 - register it as a spark sql table
+    //recordsDF.registerTempTable("sparktable")
+
+    // 3 - qry sparktable to produce another SchemaRDD object of the data needed 'finalParquet'. and persist this as parquet files
+    //val finalParquet = ss.sql("sql")
+    //finalParquet.write.saveAsTable("")
+    recordsDF.write.format("orc").mode(SaveMode.Append).saveAsTable("default.tweeter_tweets")
+
+  })
+  */
+
+  ssc.start()
+  ssc.awaitTermination()
+
+
 }
